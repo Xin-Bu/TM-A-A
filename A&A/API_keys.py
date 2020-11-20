27fa957efe4b8dc661b4314e2a98fa2e4f395e21
Xin Bu <xin.bu@umontana.edu>
2020-11-19 19:45:04 -0700

    Add files

---
+++ A&A/API_keys.py
@@ -0,0 +1,9 @@
+# api_key = 'qVwvijelkdfuhaljAxIEtPB3Tq'
+# api_key_secret = 'M29LjXVBcZtDwthfiuh842R7Q20I0HhpQlchTFLCCy2wv7c9D'
+# access_token = '419210344-gGwnP4ptrWZnkW5Ljfhaliu3FpsYOHCeo4kC73ZJ'
+# access_token_secret = '4nY4DN4e6tcemfsadsiasliu3h4PGI0etrUKG3190U3ey'
+
+api_key = "7bHA660IPZ8esPPVTBrTAk3dy"
+api_key_secret = "kwma9X6hdOZXIcEw6gKHVxe4qVbR5bXqKa7Os1KHkXatWI82vi"
+access_token = "15120246-BKwPxVuZ5cm4zZbxmllD30gngPX01rVcNcrywlnAO"
+access_token_secret = "mLeHNMFigpO9WwMdr7FgZqBmmN3YTBgn6dUZvKE58oT9p"
